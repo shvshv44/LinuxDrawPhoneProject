18e70f95db90fec824c804c24a0d62d1264f3fa7
student <sharonaizn@gmail.com>
2021-07-17 13:43:36 +0300

    adding buttons and change color

--- draw.c
+++ draw.c
@@ -19,20 +19,20 @@ typedef struct {
 	int win_height;
 	int x;
 	int y;
-} mage_state;
+} draw_state;
 
-void mage_exit(int code)
+void draw_exit(int code)
 {
 	exit(code);
 }
 
-void mage_error(const char* str)
+void draw_error(const char* str)
 {
 	printf("ERROR: %s", str);
-	mage_exit(1);
+	draw_exit(1);
 }
 
-void mage_pencil(mage_state* m, int x, int y)
+void draw_pencil(draw_state* m, int x, int y)
 {
 	uint32_t* pixels = m->screen->pixels;
 	if (SDL_MUSTLOCK(m->screen)) {
@@ -47,16 +47,30 @@ void mage_pencil(mage_state* m, int x, int y)
 	}
 }
 
-void mage_render(mage_state* m)
+void draw_rect(draw_state* m, int x, int y, int w, int h, int color) {
+	SDL_Rect rect;
+	rect.w = w;
+	rect.h = h;
+	rect.x = x;
+	rect.y = y;
+	SDL_FillRect(m->screen, &rect, color);
+}
+
+void draw_render(draw_state* m)
 {
+	int l = 32;
+	draw_rect(m, 0, 0, l, l, COLOR_BLUE);
+	draw_rect(m, l , 0, l, l, COLOR_WHITE);
+	draw_rect(m, l * 2, 0, l, l, COLOR_RED);
+	draw_rect(m, l * 3, 0, l, l, COLOR_GREEN);
 	SDL_UpdateWindowSurface(m->win);
 }
 
-mage_state* mage_init(void)
+draw_state* draw_init(void)
 {
-	mage_state* m = malloc(sizeof(*m));
+	draw_state* m = malloc(sizeof(*m));
 	if (m == NULL) {
-		mage_error("Failed to initialize state!");
+		draw_error("Failed to initialize state!");
 	}
 
 	m->color = COLOR_BLACK;
@@ -71,34 +85,34 @@ mage_state* mage_init(void)
 
 int main(int argc, char* argv[])
 {
-	mage_state* m = mage_init();
+	int xMouse, yMouse;
+	draw_state* m = draw_init();
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		mage_error("Failed to initialize SDL!");
+		draw_error("Failed to initialize SDL!");
 	}
 
-	m->win = SDL_CreateWindow("draw somethin", 0, 0, m->win_width, m->win_height, SDL_WINDOW_SHOWN);
+	m->win = SDL_CreateWindow("draw something", 0, 0, m->win_width, m->win_height, SDL_WINDOW_SHOWN);
 	if (m->win == NULL) {
-		mage_error("Failed to initialize SDL window!");
+		draw_error("Failed to initialize SDL window!");
 	}
 
 	m->screen = SDL_GetWindowSurface(m->win);
 	m->color = COLOR_WHITE;
 
-
 	/* so we don't start with a black m->screen */
-	mage_render(m);
+	draw_render(m);
 	while (1) {
 		SDL_Event e;
 		
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 			case SDL_QUIT:
-				mage_exit(0);
+				draw_exit(0);
 				break;
 			case SDL_KEYDOWN:
 				if (e.key.keysym.sym == SDLK_q || e.key.keysym.sym == SDLK_ESCAPE) {
-					mage_exit(0);
+					draw_exit(0);
 				}
 				if (e.key.keysym.sym == SDLK_1) {
 					m->color = COLOR_BLUE;
@@ -115,20 +129,42 @@ int main(int argc, char* argv[])
 				break;
 			case SDL_MOUSEBUTTONDOWN:
 				if (e.button.button == SDL_BUTTON_LEFT) {
-					mage_pencil(m, e.button.x, e.button.y);
+					SDL_GetMouseState(&xMouse,&yMouse);
+					if (xMouse < 32 && yMouse < 32) {
+						m->color = COLOR_BLUE;
+					} else if (xMouse < 64 && yMouse < 32) {
+						m->color = COLOR_WHITE;
+					} else if (xMouse < 96 && yMouse < 32) {
+						m->color = COLOR_RED;
+					} else if (xMouse < 128 && yMouse < 32) {
+						m->color = COLOR_GREEN;
+					} else {
+						draw_pencil(m, e.button.x, e.button.y);
+					}
 					m->mouse_down = 1;
 				}
 				break;
 			case SDL_MOUSEMOTION:
 				if (m->mouse_down && e.button.button == SDL_BUTTON_LEFT) {
-					mage_pencil(m, e.button.x, e.button.y);
+					SDL_GetMouseState(&xMouse,&yMouse);
+					if (xMouse < 32 && yMouse < 32) {
+						m->color = COLOR_BLUE;
+					} else if (xMouse < 64 && yMouse < 32) {
+						m->color = COLOR_WHITE;
+					} else if (xMouse < 96 && yMouse < 32) {
+						m->color = COLOR_RED;
+					} else if (xMouse < 128 && yMouse < 32) {
+						m->color = COLOR_GREEN;
+					} else {
+						draw_pencil(m, e.button.x, e.button.y);
+					}
 				}
 				break;
 			default:
 				break;
 			}
 		}
-		mage_render(m);
+		draw_render(m);
 	}
 	return 0;
 }
